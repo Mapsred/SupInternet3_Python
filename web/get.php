78fa529155d1f35696edf57a03bef59508aba554
maps_red <maps_red@yahoo.fr>
2016-10-30 20:14:54 +0100

    Add get user

--- web/get.php
+++ web/get.php
@@ -1,6 +1,6 @@
 <?php
 if (isset($_POST['crit_value'])) {
-    $url = "127.0.0.1:5000/user/".$_POST['crit_value'];
+    $url = "127.0.0.1:5000/user/".$_POST['get_value'];
     $url = str_replace(' ', '%20', $url);
     $ch = curl_init();
     curl_setopt($ch, CURLOPT_URL, $url);
@@ -10,4 +10,21 @@ if (isset($_POST['crit_value'])) {
     $parsed_json = json_decode($data);
 
     var_dump($parsed_json);
+    ?>
+
+    <!doctype html>
+    <html lang="fr">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport"
+              content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>API GET Result</title>
+    </head>
+    <body>
+    </body>
+    </html>
+
+<?php
+
 }
